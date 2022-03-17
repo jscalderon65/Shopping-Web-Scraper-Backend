c9207d711f0e677c0e1134e16fe7cc60d848e78a
jscalderon65 <jhonnseca0599@gmail.com>
2022-03-16 23:54:58 -0500

    Conexión a firebase y cloud firestore creada

---
+++ src/Db/config.js
@@ -0,0 +1,8 @@
+const firebase = require('firebase-admin')
+const {getFirestore} = require('firebase-admin/firestore')
+const serviceAccount = require('./serviceAccountKey.json')
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+})
+const db = getFirestore()
+module.exports = {db}
