b053dd34f4e461c886435460e8e7248e42c03a9a
jscalderon65 <jhonnseca0599@gmail.com>
2022-03-17 22:56:36 -0500

    Reestructuración de db

--- src/Services/crudInWatchers.services.js
+++ src/Services/crudInWatchers.services.js
@@ -5,15 +5,14 @@ const {
 const {createWatcher} = require('../Utils/watchersOperations')
 const {v4: uuidv4} = require('uuid')
 const debugAppError = require('debug')('app:error')
-const documentId = 'watchers'
-
+const collectionId = 'watchers'
 const createWatcherInProduct = async (req, res) => {
   try {
     const {userId, productBrand} = req.params
     const {productUrl} = req.body
     const IdWatcher = uuidv4()
     const newProduct = await createWatcher(productBrand, productUrl)
-    await setElementToDocument(userId, documentId, IdWatcher, newProduct)
+    await setElementToDocument(collectionId, userId, IdWatcher, newProduct)
     res.send({[IdWatcher]: newProduct})
   } catch (e) {
     res.status(500).send({error: e.message})
@@ -23,7 +22,7 @@ const createWatcherInProduct = async (req, res) => {
 const getUserWatchers = async (req, res) => {
   try {
     const {userId} = req.params
-    const data = await getElementsFromDocument(userId, documentId)
+    const data = await getElementsFromDocument(collectionId, userId)
     res.send(data)
   } catch (e) {
     res.status(500).send({error: e.message})
